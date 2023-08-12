8c3a751f7da336b1022d60d3c7825a6b62e3a111
Bebsti <129532033+Bebsti@users.noreply.github.com>
2023-08-12 15:25:01 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -11,9 +11,15 @@ bot = Client('pdiskshortner bot',
              workers=50,
              sleep_threshold=10)
 
+# Your function to get a shortened link using ClicksFly API
+async def get_shortlink(link):
+    url = 'https://clicksfly.com/api'
+    params = {'api': API_KEY, 'url': link}
 
-
-
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, params=params, raise_for_status=True) as response:
+            data = await response.json()
+            return data["shortenedUrl"]
 
 # Your handler function for the "genlink" and "batch" commands
 @bot.on_message(filters.private & filters.user(ADMINS) & filters.command(['genlink', 'batch']))
@@ -34,31 +40,20 @@ async def link_handler(client: Client, message: Message):
         if msg_id:
             break
         else:
-            await channel_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is not taken from DB Channel", quote=True)
+            await channel_message.reply("❌ Error\n\nThis Forwarded Post is not from my DB Channel or this Link is not taken from DB Channel", quote=True)
             continue
 
     base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
     link = f"https://t.me/{client.username}?start={base64_string}"
 
-
-  # Your function to get a shortened link using ClicksFly API
-   async def get_shortlink(link1):
-    url = 'https://clicksfly.com/api'
-    params = {'api': API_KEY, 'url': link}
-
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, params=params, raise_for_status=True) as response:
-            data = await response.json()
-            return data["shortenedUrl"]
-
     try:
-        short_link = await get_shortlink(link1)  # Call get_shortlink function here
+        short_link = await get_shortlink(link)  # Call get_shortlink function here
+
+        # Use the short_link in your code
         reply_markup = InlineKeyboardMarkup([
             [InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={short_link}')]
         ])
         await channel_message.reply_text(f"<b>Here is your link</b>\n\n{short_link}", quote=True, reply_markup=reply_markup)
     except Exception as e:
         await channel_message.reply(f'Error: {e}', quote=True)
-
-
 
